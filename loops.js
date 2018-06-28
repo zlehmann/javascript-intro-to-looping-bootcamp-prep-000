43098ed2b5cb246fbe8785ff17747d79cc6d542d
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 15:22:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(arr) {
   for (let i = 0; i<25; i++) {
     if (i==1){
-      arr[i] = `"I am ${i} strange loop."`
+      arr.push(`"I am ${i} strange loop."`)
     } else {
-      arr[i] = `"I am ${i} strange loops."`
+      arr.push(`"I am ${i} strange loops."`)
     }
   }
   return arr
